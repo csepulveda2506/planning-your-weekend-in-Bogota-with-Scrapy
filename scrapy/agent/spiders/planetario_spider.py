09584d6eed15352e82a35150b1124c39dd6e5a58
camilo <camilo.sepulveda@endava.com>
2019-02-06 21:11:12 -0500

    Add Scrapy crawler to complete data parsing. Production to Kafka

--- scrapy/agent/spiders/planetario_spider.py
+++ scrapy/agent/spiders/planetario_spider.py
@@ -1,6 +1,15 @@
 import datetime
+import re
+from json import dumps
+
+from kafka import KafkaProducer
 
 import scrapy
+
+KAFKA_TOPIC = 'pycon-test-topic'
+KAFKA_BROKERS = 'localhost:9092'
+producer = KafkaProducer(bootstrap_servers=[KAFKA_BROKERS],
+                         value_serializer=lambda x: dumps(x).encode('utf-8'))
 
 MONTH, YEAR = datetime.datetime.now().month, datetime.datetime.now().year
 
@@ -17,12 +26,31 @@ class PlanetarioSpider(scrapy.Spider):
             time = event.css(
                 'div.hora_evento span.date-display-single::text').extract_first()
             date = getDate(event)
-            message = {
-                'start-date': date,
-                'time': time,
-                'link': link
-            }
+            message = scrapy.Request(
+                "http://www.planetariodebogota.gov.co" + link,
+                callback=self.parse_event,
+                meta={'link': link, 'time': time, 'date': date})
             yield message
+
+    def parse_event(self, response):
+        title = response.xpath(
+            "//div[@id='titulo-evento']/text()").extract_first()
+        place = response.xpath(
+            "//div[@id='lugar-evento']/text()").extract_first()
+        description = response.xpath(
+            "//div[@id='cuerpo-evento']/p/text()").extract_first()
+        price = getPrice(response.xpath("//div[@id='cuerpo-evento']/p/text()"))
+        message = {
+            'title': title,
+            'description': description,
+            'location': place,
+            'start-date': response.meta['date'],
+            'time': response.meta['time'],
+            'price': price,
+            'link': response.meta['link']
+        }
+        producer.send(topic=KAFKA_TOPIC, value=message)
+        return message
 
 
 def getDate(event):
@@ -33,3 +61,11 @@ def getDate(event):
             'div.dia_evento span.date-display-start::text').extract_first()
     day = str(date).split()
     return day[1] + "-" + str(MONTH) + "-" + str(YEAR)
+
+
+def getPrice(price):
+    for paragraph in price:
+        part = paragraph.extract()
+        if "$" in part:
+            return int(re.search('\$(.+?),', part).group(1).replace(".", ""))
+    return 0
